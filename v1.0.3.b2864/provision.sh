9b9d150d13793cec9bded3bc831e6bb39716a3b6
gomesuit <gomesuit@gmail.com>
2017-06-10 22:56:27 +0900

    コメント削除

--- v1.0.3.b2864/provision.sh
+++ v1.0.3.b2864/provision.sh
@@ -22,28 +22,3 @@ systemctl start postgresql-9.6
 cd /vagrant
 docker-compose run --rm server create_db
 docker-compose up -d
-
-## create database
-#cd /vagrant
-#
-#run_redash="docker-compose run --rm redash"
-#
-#$run_redash /opt/redash/current/manage.py database create_tables
-#
-## Create default admin user
-#$run_redash /opt/redash/current/manage.py users create --admin --password admin "Admin" "admin"
-#
-#run_psql="sudo -u postgres psql -h 192.168.33.60"
-#
-## Create redash_reader user. We don't use a strong password, as the instance supposed to be accesible only from the redash host.
-#$run_psql -c "CREATE ROLE redash_reader WITH PASSWORD 'redash_reader' NOCREATEROLE NOCREATEDB NOSUPERUSER LOGIN"
-#$run_psql -c "grant select(id,name,type) ON data_sources to redash_reader;"
-#$run_psql -c "grant select(id,name) ON users to redash_reader;"
-#$run_psql -c "grant select on events, queries, dashboards, widgets, visualizations, query_results to redash_reader;"
-#
-#$run_redash /opt/redash/current/manage.py ds new "Redash Metadata" --type "pg" --options "{\"user\": \"redash_reader\", \"password\": \"redash_reader\", \"host\": \"postgres\", \"dbname\": \"postgres\"}"
-#
-#docker-compose up -d
-
-
-
